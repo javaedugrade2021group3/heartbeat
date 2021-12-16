b4fd3d81a81d11621a13be73953715b6741c1dbf
Isabelle sjölund <isabellesjolund@Isabelles-MacBook-Pro.local>
2021-12-16 15:08:45 +0100

    changed view

--- src/main/java/com/edugrade/heartbeat/Main/Main.java
+++ src/main/java/com/edugrade/heartbeat/Main/Main.java
@@ -11,9 +11,10 @@ import java.io.IOException;
 public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        //Parent root = FXMLLoader.load(getClass().getResource("/View/newloginview.fxml"));
-        Parent root = FXMLLoader.load(getClass().getResource("/View/logged-in-view.fxml"));
-        Scene scene = new Scene(root, 800, 500);
+       Parent root = FXMLLoader.load(getClass().getResource("/View/newloginview.fxml"));
+        Parent root1 = FXMLLoader.load(getClass().getResource("/View/logged-in-view.fxml"));
+        Scene scene = new Scene(root, 1000, 600);
+
         stage.setTitle("Rentals4You");
         stage.setScene(scene);
         stage.show();
